34eae2b46c0efbfb369bc45f6360713409c68acd
Peter Pardo <peterpardo123@gmail.com>
2024-01-23 15:33:13 +0800

    feat: added description field

--- app/components/PostForm.tsx
+++ app/components/PostForm.tsx
@@ -1,7 +1,22 @@
+"use client";
+
 import TextField from "@/components/TextField";
-import React from "react";
+import React, { useState } from "react";
 
 const PostForm = () => {
+  const [postData, setPostData] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setPostData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <div className="space-y-5">
       {/* Title */}
@@ -23,12 +38,16 @@ const PostForm = () => {
         id="title"
         label="Title"
         placeholder="Your post title here..."
+        value={postData.title}
+        onChange={handleInputChange}
       />
       <TextField
         name="description"
         id="description"
         label="Description"
         placeholder="Your post description here..."
+        value={postData.description}
+        onChange={handleInputChange}
       />
     </div>
   );
