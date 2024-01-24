2e0e321989c16095c0bafc84d478d01c6ffa5869
Peter Pardo <peterpardo123@gmail.com>
2024-01-25 02:04:20 +0800

    feat: displayed posts in home page

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 
-export default function BlogCard() {
+type BlogCard = {
+  title: string;
+  description: string;
+  author: string;
+};
+
+export default function BlogCard({ title, description, author }: BlogCard) {
   return (
     <div className="max-w-96 max-h-[400px] mx-auto rounded-lg shadow-lg">
       <Image
@@ -11,13 +17,9 @@ export default function BlogCard() {
         className="rounded-t-lg"
       />
       <div className="p-4">
-        {/* title */}
-        <h1 className="font-bold text-lg">How to train your cat.</h1>
+        <h1 className="font-bold text-lg">{title}</h1>
         <p className="text-sm text-gray-500">By Peter Pardo - Jan 19, 2023</p>
-        <p className="text-sm mt-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          eius quo adipisci numquam ab quidem.
-        </p>
+        <p className="text-sm mt-2">{description}</p>
       </div>
     </div>
   );
--- app/components/BlogList.tsx
+++ app/components/BlogList.tsx
@@ -1,14 +1,25 @@
 import BlogCard from "@/components/BlogCard";
+import { prisma } from "@/utils/prismaClient";
 
-export default function BlogList() {
+export default async function BlogList() {
+  const posts = await prisma.post.findMany();
+
   return (
     <div className="grid py-5 grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
-      <BlogCard />
+      {posts.length > 0 ? (
+        posts.map((post) => (
+          <>
+            <BlogCard
+              key={post.id}
+              title={post.title}
+              description={post.description}
+              author={post.authorId}
+            />
+          </>
+        ))
+      ) : (
+        <p>No posts available</p>
+      )}
     </div>
   );
 }
