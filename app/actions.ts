c5552c0a6d5e33bd9f8b3bc6c1152207f2bd6156
Peter Pardo <peterpardo123@gmail.com>
2024-01-25 15:35:09 +0800

    feat: store image file on create post

--- app/actions.ts
+++ app/actions.ts
@@ -1,10 +1,10 @@
 "use server";
 
 import { auth } from "@clerk/nextjs";
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/utils/db";
 import { redirect } from "next/navigation";
-
-const prisma = new PrismaClient();
+import { supabase } from "@/utils/storage";
+import { v4 as uuidv4 } from "uuid";
 
 export async function createPost(_: any, formData: FormData) {
   try {
@@ -33,32 +33,49 @@ export async function createPost(_: any, formData: FormData) {
       newErrors.content = "Content is required";
     }
 
-    if ((formData.get("image") as File).size === 0) {
+    const imageFile = formData.get("image") as File;
+    const fileSize = imageFile.size / 1024 ** 2; // convert file size to MB
+    if (imageFile.size === 0) {
       newErrors.image = "Image is required";
+    } else if (fileSize > 5) {
+      newErrors.image = "Image file must be less than 5mb";
     }
 
-    if (Object.keys(newErrors).length === 0) {
+    if (Object.keys(newErrors).length > 0) {
       return {
         ...errorMessages,
         ...newErrors,
       };
     }
 
-    // const post = await prisma.post.create({
-    //   data: {
-    //     title: formData.get("title") as string,
-    //     description: formData.get("description") as string,
-    //     content: formData.get("content") as string,
-    //     authorId: userId,
-    //     published: false,
-    //     image: "sample image",
-    //   },
-    // });
-    // console.log("post created", post);
+    // Store image file in storage
+    const imageExt = imageFile.type.split("/")[1];
+    const fileName = `${userId}_${uuidv4()}.${imageExt}`;
+
+    const { data: storageFile, error } = await supabase.storage
+      .from("next-blog-storage")
+      .upload(fileName, imageFile);
+
+    if (error) {
+      throw new Error("Error occurred. Try again.");
+    }
+
+    const post = await prisma.post.create({
+      data: {
+        title: formData.get("title") as string,
+        description: formData.get("description") as string,
+        content: formData.get("content") as string,
+        authorId: userId,
+        published: Boolean(formData.get("publish")),
+        image: storageFile.path,
+      },
+    });
+
+    console.log("post created", post);
   } catch (e) {
     console.log("Error:", e);
     return;
   }
 
-  // redirect("/");
+  redirect("/");
 }
--- app/components/BlogList.tsx
+++ app/components/BlogList.tsx
@@ -10,14 +10,12 @@ export default async function BlogList() {
     <div className="grid py-5 grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
       {posts.length > 0 ? (
         posts.map((post) => (
-          <>
-            <BlogCard
-              key={post.id}
-              title={post.title}
-              description={post.description}
-              author={post.authorId}
-            />
-          </>
+          <BlogCard
+            key={post.id}
+            title={post.title}
+            description={post.description}
+            author={post.authorId}
+          />
         ))
       ) : (
         <p>No posts available</p>
