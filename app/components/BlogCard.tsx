0aa402808dcc506b0fd68c62e32df34759947154
Peter Pardo <peterpardo123@gmail.com>
2024-01-25 16:16:30 +0800

    feat: updated title and description length shown in blog card

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -2,12 +2,27 @@ import { supabase } from "@/utils/storage";
 import { Post } from "@prisma/client";
 import Image from "next/image";
 import dayjs from "dayjs";
+import Link from "next/link";
 
 export default function BlogCard({ post }: { post: Post }) {
   const { data } = supabase.storage
     .from("next-blog-storage")
     .getPublicUrl(post.image);
+    
   const formattedDate = dayjs(post.createdAt).format("MMM D, YYYY");
+  const formattedTitle =
+    post.title.length > 73 ? `${post.title.slice(0, 73)}...` : post.title;
+  const formattedDescription =
+    post.description.length > 30 ? (
+      <>
+        {post.description.slice(0, 30)}...{" "}
+        <Link href="/" className="text-blue-600 hover:text-blue-500">
+          read more
+        </Link>
+      </>
+    ) : (
+      post.description
+    );
 
   return (
     <div className="max-w-96 max-h-[400px] mx-auto rounded-lg shadow-lg">
@@ -19,11 +34,11 @@ export default function BlogCard({ post }: { post: Post }) {
         className="rounded-t-lg"
       />
       <div className="p-4">
-        <h1 className="font-bold text-lg">{post.title}</h1>
+        <h1 className="font-bold text-lg">{formattedTitle}</h1>
         <p className="text-sm text-gray-500">
           By {post.authorName} - {formattedDate}
         </p>
-        <p className="text-sm mt-2">{post.description}</p>
+        <p className="text-sm mt-2">{formattedDescription}</p>
       </div>
     </div>
   );
