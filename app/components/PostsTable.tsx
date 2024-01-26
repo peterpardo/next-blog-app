760f5a2bee0af4af6f151300655dde7c8ee6cb75
Peter Pardo <peterpardo123@gmail.com>
2024-01-27 02:12:53 +0800

    feat: formatted other columns and added action column

--- app/components/PostsTable.tsx
+++ app/components/PostsTable.tsx
@@ -22,12 +22,12 @@ export default function PostsTable({
 
   return (
     <div className="w-full overflow-x-auto rounded-lg border border-b">
-      <table className="table-auto min-w-[800px] min-h-[500px] border-collapse text-left">
+      <table className="table-auto w-full min-w-[800px] min-h-[500px] border-collapse text-left">
         <thead>
           {table.getHeaderGroups()?.map((headerGroup) => (
-            <tr key={headerGroup.id}>
+            <tr className="bg-slate-100" key={headerGroup.id}>
               {headerGroup.headers.map((header) => (
-                <th key={header.id}>
+                <th className="px-2 py-1" key={header.id}>
                   {flexRender(
                     header.column.columnDef.header,
                     header.getContext()
@@ -41,7 +41,7 @@ export default function PostsTable({
           {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
               {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
+                <td className="px-2 py-1" key={cell.id}>
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               ))}
--- app/utils/postColumns.tsx
+++ app/utils/postColumns.tsx
@@ -11,21 +11,27 @@ const columnHelper = createColumnHelper<Post>();
 export const postColumns = [
   columnHelper.accessor("id", {
     header: "Post Id",
+    cell: (props) => {
+      return <div className="text-center">{props.getValue()}</div>;
+    },
   }),
   columnHelper.accessor("title", {
     header: "Title",
+    cell: (props) => {
+      return <div className="max-w-72">{props.getValue()}</div>;
+    },
   }),
   columnHelper.accessor("image", {
     header: "Image",
-    cell: (info) => {
+    cell: (props) => {
       const { data } = supabase.storage
         .from("next-blog-storage")
-        .getPublicUrl(info.getValue());
+        .getPublicUrl(props.getValue());
 
       return (
         <Image
           src={data.publicUrl}
-          alt={info.row.original.title}
+          alt={props.row.original.title}
           width={200}
           height={200}
           loading="lazy"
@@ -35,8 +41,8 @@ export const postColumns = [
   }),
   columnHelper.accessor("description", {
     header: "Description",
-    cell: (info) => {
-      const desc = info.getValue();
+    cell: (props) => {
+      const desc = props.getValue();
       const formattedDescription =
         desc.length > 30 ? `${desc.slice(0, 30)}...` : desc;
 
@@ -45,9 +51,30 @@ export const postColumns = [
   }),
   columnHelper.accessor("createdAt", {
     header: "Created At",
-    cell: (info) => {
-      const formattedDate = dayjs(info.getValue()).format("MMM D, YYYY");
+    cell: (props) => {
+      const formattedDate = dayjs(props.getValue()).format("MMM D, YYYY");
       return formattedDate;
+    },
+  }),
+  columnHelper.display({
+    id: "actions",
+    cell: (props) => {
+      return (
+        <div className="flex items-center gap-x-2">
+          <button
+            onClick={() => console.log("Edit: ", props.row.original.id)}
+            className="px-2 py-1 rounded-lg text-xs bg-slate-100 text-gray-600 hover:bg-slate-50"
+          >
+            edit
+          </button>
+          <button
+            onClick={() => console.log("Delete: ", props.row.original.id)}
+            className="px-2 py-1 rounded-lg text-xs bg-slate-100 text-gray-600 hover:bg-slate-50"
+          >
+            delete
+          </button>
+        </div>
+      );
     },
   }),
 ];
