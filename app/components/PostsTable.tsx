25e91837dd4f5969a7cc4b82abe1640720d38a30
Peter Pardo <peterpardo123@gmail.com>
2024-01-27 20:16:32 +0800

    feat: added pagination in posts table

--- app/components/PostsTable.tsx
+++ app/components/PostsTable.tsx
@@ -4,6 +4,7 @@ import { Post } from "@prisma/client";
 import {
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -18,10 +19,19 @@ export default function PostsTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+
+    initialState: {
+      pagination: {
+        pageSize: 3,
+      },
+    },
   });
 
+  console.log(table.getState());
+
   return (
-    <div className="w-full overflow-x-auto rounded-lg border border-b">
+    <div className="w-full overflow-x-auto rounded-lg border border-b p-2">
       <table className="table-auto w-full min-w-[800px] min-h-[500px] border-collapse text-left">
         <thead>
           {table.getHeaderGroups()?.map((headerGroup) => (
@@ -49,6 +59,30 @@ export default function PostsTable({
           ))}
         </tbody>
       </table>
+
+      <div className="flex justify-end items-center gap-2">
+        <button
+          className="border rounded px-4 py-2"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<"}
+        </button>
+        <button
+          className="border rounded px-4 py-2"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {">"}
+        </button>
+        <span className="flex items-center gap-1">
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </strong>
+        </span>
+      </div>
     </div>
   );
 }
