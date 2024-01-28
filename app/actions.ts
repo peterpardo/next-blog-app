eab669927e38bf8232240fed17300f97d615dce3
Peter Pardo <peterpardo123@gmail.com>
2024-01-28 22:20:03 +0800

    refactor: validation and file upload in creating a post

--- app/actions.ts
+++ app/actions.ts
@@ -6,6 +6,13 @@ import { redirect } from "next/navigation";
 import { supabase } from "@/utils/storage";
 import { v4 as uuidv4 } from "uuid";
 
+type ErrorMessages = {
+  title: string;
+  description: string;
+  content: string;
+  image: string;
+};
+
 export async function createPost(_: any, formData: FormData) {
   try {
     const user = await currentUser();
@@ -15,31 +22,12 @@ export async function createPost(_: any, formData: FormData) {
       content: "",
       image: "",
     };
-    let newErrors = {} as typeof errorMessages;
 
     if (!user) {
       throw new Error("User not authenticated.");
     }
 
-    if (formData.get("title") === "") {
-      newErrors.title = "Title is required";
-    }
-
-    if (formData.get("description") === "") {
-      newErrors.description = "Description is required";
-    }
-
-    if (formData.get("content") === "") {
-      newErrors.content = "Content is required";
-    }
-
-    const imageFile = formData.get("image") as File;
-    const fileSize = imageFile.size / 1024 ** 2; // convert file size to MB
-    if (imageFile.size === 0) {
-      newErrors.image = "Image is required";
-    } else if (fileSize > 5) {
-      newErrors.image = "Image file must be less than 5mb";
-    }
+    let newErrors = validatePost(formData);
 
     if (Object.keys(newErrors).length > 0) {
       return {
@@ -48,17 +36,22 @@ export async function createPost(_: any, formData: FormData) {
       };
     }
 
+    const imageFile = await storePostImage(
+      formData.get("image") as File,
+      user.id
+    );
     // Store image file in storage
-    const imageExt = imageFile.type.split("/")[1];
-    const fileName = `${user.id}_${uuidv4()}.${imageExt}`;
+    // const imageFile = formData.get("image") as File;
+    // const imageExt = imageFile.type.split("/")[1];
+    // const fileName = `${user.id}_${uuidv4()}.${imageExt}`;
 
-    const { data: storageFile, error } = await supabase.storage
-      .from("next-blog-storage")
-      .upload(fileName, imageFile);
+    // const { data: storageFile, error } = await supabase.storage
+    //   .from("next-blog-storage")
+    //   .upload(fileName, imageFile);
 
-    if (error) {
-      throw new Error("Error occurred. Try again.");
-    }
+    // if (error) {
+    //   throw new Error("Error occurred. Try again.");
+    // }
 
     const post = await prisma.post.create({
       data: {
@@ -69,15 +62,63 @@ export async function createPost(_: any, formData: FormData) {
         authorName: `${user.firstName} ${user.lastName}`,
         authorImage: user.hasImage ? user?.imageUrl : "",
         published: Boolean(formData.get("publish")),
-        image: storageFile.path,
+        image: imageFile.path,
       },
     });
 
     console.log("post created", post);
   } catch (e) {
     console.log("Error:", e);
-    return;
+    throw new Error("Something went wrong. Try again.");
   }
 
   redirect("/my-posts");
+}
+
+function validatePost(formData: FormData) {
+  let newErrors = {} as ErrorMessages;
+
+  if (formData.get("title") === "") {
+    newErrors.title = "Title is required";
+  }
+
+  if (formData.get("description") === "") {
+    newErrors.description = "Description is required";
+  }
+
+  if (formData.get("content") === "") {
+    newErrors.content = "Content is required";
+  }
+
+  const imageFile = formData.get("image") as File;
+  const fileSize = imageFile.size / 1024 ** 2; // convert file size to MB
+  if (imageFile.size === 0) {
+    newErrors.image = "Image is required";
+  } else if (fileSize > 5) {
+    newErrors.image = "Image file must be less than 5mb";
+  }
+
+  return newErrors;
+}
+
+async function storePostImage(
+  imageFile: File,
+  userId: string
+): Promise<{ path: string }> {
+  try {
+    const imageExt = imageFile.type.split("/")[1];
+    const fileName = `${userId}_${uuidv4()}.${imageExt}`;
+
+    const { data: storageFile, error } = await supabase.storage
+      .from("next-blog-storage")
+      .upload(fileName, imageFile);
+
+    if (error) {
+      throw new Error("Error occurred on image upload");
+    }
+
+    return storageFile;
+  } catch (e) {
+    throw new Error("Error occurred on image upload");
+  }
 }
